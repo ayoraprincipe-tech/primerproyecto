f9c00c053a1b7edd473ce1e8ae25f17ae9e0d8eb
ayoraprincipe-tech <ayoraprincipe@gmail.com>
2025-10-17 23:18:53 -0500

    Actualiza funciones y base de datos

--- funciones.py
+++ funciones.py
@@ -1,5 +1,6 @@
 
 import sqlite3 as sql
+from tkinter import messagebox
 
 
 class usuarios():
@@ -7,11 +8,22 @@ class usuarios():
         self.usuario = usuario
         self.contraseña = contraseña
         self.ruta_db = r"C:\gabriel\Mipropioproyecto.py\usuariosycontraseñas.db"
+        self.useryaregris = f"SELECT * FROM usuarios WHERE (?)", (self.usuario)
 
     def regristar_usuario(self):
         conn = sql.connect(self.ruta_db)
         cursor = conn.cursor()
-        cursor.execute(
-            f"INSERT INTO usuariosycontraseñas VALUES (?,?)", (self.usuario, self.contraseña))
+        if (self.usuario == self.useryaregris):
+            cursor.execute(
+                f"INSERT INTO usuariosycontraseñas VALUES (?,?)", (self.usuario, self.contraseña))
+        else:
+            messagebox.showinfo(
+                "Información", "Usuario ya regristado, pruebe con otro nombre")
         conn.commit()
         conn.close()
+
+    def eliminar_usuario(self):
+        conn = sql.connect(self.ruta_db)
+        cursor = conn.cursor()
+        cursor.execute(
+            "DELETE FROM Usuario ya regristado WHERE (?)", self.usuario)
--- login.py
+++ login.py
@@ -1,17 +1,21 @@
 # pseudocodigo primero crear mi primeraventana tk, luego crear un label que diga Bienvenido ingresa tu usuario y contraseña
 import tkinter as tk
 from funciones import usuarios
+from tkinter import messagebox
 
 first_ventana = tk.Tk()
 first_ventana.title("Proyecto1")
 first_ventana.geometry("1920x1080")
 
 
-def registrar_usuario():
-    usuario = entrada1.get()
-    contraseña = entrada2.get()
-    sesion = usuarios(usuario, contraseña)
-    sesion.regristar_usuario()
+def registrar_user():
+    if (aceptarpoliticas.get()):
+        usuario = entrada1.get()
+        contraseña = entrada2.get()
+        sesion = usuarios(usuario, contraseña)
+        return sesion.regristar_usuario()
+    else:
+        return messagebox.showinfo("Información", "acepte las politicas de privacidad")
 
 
 label1 = tk.Label(
@@ -20,28 +24,37 @@ label1.config(font=("arial", 24, "bold"))
 label1.pack()
 
 
-label2 = tk.Label(first_ventana, text="Ingresa tu usuario")
+label2 = tk.Label(first_ventana, text="Ingresa tu usuario",
+                  font=("arial", 12, "bold"))
 label2.pack()
 
 
-entrada1 = tk.Entry(bg="gray", fg="blue")
+entrada1 = tk.Entry(bg="gray", fg="black", font=(
+    "arial", 12), text="user here:")
 entrada1.pack()
 
 
-label3 = tk.Label(first_ventana, text="Ingresa tu contraseña")
+label3 = tk.Label(first_ventana, text="Ingresa tu contraseña",
+                  font=("arial", 12, "bold"))
 label3.pack()
 
 
-entrada2 = tk.Entry(bg="gray", fg="blue")
+entrada2 = tk.Entry(bg="gray", fg="black", font=("arial", 12))
 entrada2.pack()
 
+aceptarpoliticas = tk.BooleanVar()
+Politicas_check = tk.Checkbutton(
+    first_ventana, text="Acepta las politicas de privacidad", font=("arial", 12, "bold"), variable=aceptarpoliticas)
+Politicas_check.pack()
 
-button1 = tk.Button(text="Iniciar sesion", command=registrar_usuario)
-button1.pack()
+Iniciar_sesion = tk.Button(
+    text="Iniciar sesion", font=("arial", 12, "bold"))
+Iniciar_sesion.pack()
 
 
-button2 = tk.Button(text="Registarse")
-button2.pack()
+Regristate_buton = tk.Button(
+    text="Registarse", command=registrar_user, font=("arial", 12, "bold"))
+Regristate_buton.pack()
 
 
 first_ventana.mainloop()
